854770726bc4a2f37393131fff480e22bf297d23
Yevhen Protasov <protasovyevhen@gmail.com>
2024-06-18 16:38:30 +0300

    make build

---
+++ index.bd94f204.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");if(r){var l=r.href;t.setAttribute("src",l)}});
+//# sourceMappingURL=index.bd94f204.js.map
